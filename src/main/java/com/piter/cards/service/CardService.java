81bc891c1d69329a5ef68bba236529f003b74d3f
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-06-08 18:29:31 -0300

    [CardService] methods save, delete and listAll

--- src/main/java/com/piter/cards/service/CardService.java
+++ src/main/java/com/piter/cards/service/CardService.java
@@ -1,12 +1,17 @@
 package com.piter.cards.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.piter.cards.dto.card.CardDTO;
 import com.piter.cards.mapper.GenericMapper;
+import com.piter.cards.mapper.requests.ResponseModel;
+import com.piter.cards.model.Card;
 import com.piter.cards.repository.CardRepository;
 
 @Service
@@ -17,5 +22,64 @@ public class CardService {
 	
 	@Autowired
 	private GenericMapper mapper;
+	
+	
+	public ResponseModel<CardDTO> save(CardDTO card) {
+		
+		ResponseModel<CardDTO> response;
+		
+		try {
+			
+			//mapper from CardDTO to Card that is the parameter type for repository
+			Card savedCard = repository.save( mapper.toObject(card, Card.class) );
+			
+			response = new ResponseModel<>("SUCCESS");
+			response.setMessage("Card has been succesfully saved");
+			
+			//mapper from Card to CardDTO that is the parameter type for response
+			response.setData( mapper.toObject(savedCard, CardDTO.class) );
+			
+		} catch (Exception e) {
+			response = new ResponseModel<>("Error");
+			response.setMessage("Can't save the Card");
+		}
+		
+		return response;
+		
+	}
+	
+	public ResponseModel<CardDTO> listAll() {
+		
+		ResponseModel<CardDTO> response;
+		
+		try {
+			List<Card> cards = repository.findAll();
+			response = new ResponseModel<>("SUCCESS");
+			response.setList(mapper.toList(cards, CardDTO.class));
+		} catch (Exception e) {
+			response = new ResponseModel<>("Error");
+			response.setMessage("Can't list Cards");
+		}
+		
+		return response;
+		
+	}
+		
+	public ResponseModel<Void> delete(Long id) {
+		
+		ResponseModel<Void> response;
+		
+		try {
+			repository.deleteById(id);
+			response = new ResponseModel<>("SUCCESS");
+			response.setMessage("Card has been succesfully removed");
+		} catch (Exception e) {
+			response = new ResponseModel<>("Error");
+			response.setMessage("Can't remove the Card");
+		}
+		
+		return response;
+		
+	}
 	
 }
